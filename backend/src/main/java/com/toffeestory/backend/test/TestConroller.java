19890d36b9758e639eee3443d5719792d4cdf96b
Vincent <gunflake09@gmail.com>
2019-11-12 00:36:48 +0900

    Login 페이지 추가 및 BackEnd 서버 데이터 통신 테스트 성공
    ES-Lint 규칙으로 인해 package.json Rules에 Indent 제한 해제

--- backend/src/main/java/com/toffeestory/backend/test/TestConroller.java
+++ backend/src/main/java/com/toffeestory/backend/test/TestConroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestConroller {
 
-    @GetMapping("/hello")
+    @GetMapping("/api/hello")
     public String getHello(){
         return "hello";
     }
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>{{info}}</h2>
+  </div>
+</template>
+
+<script>
+
+  import axios from 'axios'
+
+  export default {
+    data () {
+      return {
+        info: null
+      }
+    },
+    mounted () {
+      axios
+        .get('api/hello')
+        .then(response => (this.info = response.data))
+    }
+  }
+
+</script>
