86d4ffdf3fd5dd2666d2d6c3a797f6c992b0fffd
Vincent <gunflake09@gmail.com>
2019-11-13 00:57:03 +0900

    Tailwind CSS 적용

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an Login page</h1>
     <h2>{{info}}</h2>
+    <p class="bg-blue-200 font-extrabold">
+      hello
+    </p>
   </div>
 </template>
 
 <script>
-
-  import axios from 'axios'
+  import '../assets/css/tailwind.css'
+  // import axios from 'axios'
 
   export default {
     data () {
       return {
         info: null
       }
-    },
-    mounted () {
-      axios
-        .get('api/hello')
-        .then(response => (this.info = response.data))
     }
+
+    // ,
+    // mounted () {
+    //   axios
+    //     .get('api/hello')
+    //     .then(response => (this.info = response.data))
+    // }
   }
 
 </script>
