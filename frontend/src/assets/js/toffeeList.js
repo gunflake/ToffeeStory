38782506298b7d3964f94f4f70524eb5c7f5f734
yuri <yulyuleee@gmail.com>
2019-11-25 16:06:40 +0900

    home 페이지 이미지 뿌리기 (정렬 전)

--- frontend/src/assets/js/toffeeList.js
+++ frontend/src/assets/js/toffeeList.js
@@ -11,21 +11,30 @@ export default {
       loadMore: true,
       page: 1,
       pageSize: 9,
-      images: []
+      images: [],
+      masksHide: []
     }
   },
   methods: {
     getImagesInfo () {
-      axios.get('https://api.unsplash.com/photos?page=7&per_page=9&client_id=e874834b096dcd107c232fe4b0bb521158b62e486580c988b0a75cb0b77a2abe')
+      axios.get('https://api.unsplash.com/photos', {
+        params: {
+          page: this.page++,
+          per_page: this.pageSize,
+          client_id: 'e874834b096dcd107c232fe4b0bb521158b62e486580c988b0a75cb0b77a2abe'
+        }
+      })
         .then(res => {
-          this.images.concat(res.data)
-
-          // Stop scroll-loader
-          res.data.length < this.pageSize && (this.loadMore = false)
+          res.data && res.data.length && (this.images = this.images.concat(res.data))
         })
         .catch(error => {
           console.log(error)
         })
+    }
+  },
+  watch: {
+    page (n) {
+      n > 30 && (this.loadMore = false)
     }
   },
   mounted () {
--- frontend/src/components/ToffeeList.vue
+++ frontend/src/components/ToffeeList.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="image-container">
-  <scroll-loader :loader-method="getImagesInfo" :loader-enable="loadMore">
-  </scroll-loader>
-
-  <!-- Replace the default loading animation with slot -->
-  <scroll-loader :loader-method="getImagesInfo" :loader-enable="loadMore">
-    <div>Loading...</div>
-  </scroll-loader>
-</div>
+  <div>
+    <div class="images-container">
+      <div class="images-item" v-for="(image,index) of images" :key="index">
+        <div class="images-card">
+          <img class="images-card__image" :src="image.urls.small" @load="masksHide.push(index)">
+          <div class="images-card__mask" :style="{'background-color':image.color}" v-if="!masksHide.includes(index)"></div>
+        </div>
+      </div>
+    </div>
+    <scroll-loader :loader-method="getImagesInfo" :loader-enable="loadMore" loader-color="rgba(102,102,102,.5)">
+    </scroll-loader>
+  </div>
 </template>
 <script src="../assets/js/toffeeList.js"></script>
-<script>
-
-</script>
