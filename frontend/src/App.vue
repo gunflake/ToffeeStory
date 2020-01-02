f376bcd61f39d6e1dcf30aea5e577bc26e63c924
Vincent <gunflake09@gmail.com>
2020-01-02 19:08:04 +0900

    Alert 파일명 변경 및 변수 제거

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <div id="app">
     <!-- FindPassword Alert -->
-    <FindPwdAlert :state="alertState"></FindPwdAlert>
+    <Alert/>
     <!-- state = 1 로그인, 0 = 로그아웃 -->
-    <Header :state="state" userId="hmnam"></Header>
+    <Header/>
     <router-view/>
   </div>
 </template>
 
 <script>
   import Header from '@/layout/Header'
-  import FindPwdAlert from '@/components/FindPwdAlert'
+  import Alert from '@/components/Alert'
   export default {
-    data: function () {
-      return {
-        alertState: 1,
-        state: 0
-      }
-    },
     components: {
       Header,
-      FindPwdAlert
+      Alert
     }
   }
 </script>
