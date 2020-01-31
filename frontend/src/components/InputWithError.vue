3286c3f7b75656b1ec8e82f96f0334d3d1a6386f
sera0330 <seralee0330@gmail.com>
2020-01-31 14:56:39 +0900

    InputWithError Component 추가

---
+++ frontend/src/components/InputWithError.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="mb-4">
+    <div class="text-base text-left mb-2">{{ title }}</div>
+    <input
+      v-bind:type="type"
+      v-bind:id="id"
+      v-model="value"
+      @keydown="eventFunction"
+      @keyup="eventFunction"
+      @keypress="eventFunction"
+      v-bind:class="{ 'border-red-700' : visible }"
+      class="block border border-gray-700 w-full p-2 rounded"/>
+    <div v-show="visible" class="form-error-inline">{{ errorMsg }}</div>
+  </div>
+</template>
+<style>
+  .form-error-inline {
+    margin-top: 6px;
+    padding: 3px 9px;
+    font-size: 12px;
+    border-radius: 4px;
+    background-color: #f9ded8;
+  }
+  input:focus {
+    outline: none;
+  }
+</style>
+<script>
+  export default {
+    name: 'InputWithError',
+    props: {
+      type: String,
+      title: String,
+      id: String,
+      visible: Boolean,
+      errorMsg: String
+    },
+    data () {
+      return {
+        value: ''
+      }
+    },
+    methods: {
+      eventFunction () {
+        this.$emit('inputVal', this.value)
+      }
+    }
+  }
+</script>
