0fb28390b96827e7d56af24dcdbfd76dc9ab8bbc
Vincent <gunflake09@gmail.com>
2020-02-26 12:57:42 +0900

    Post 엔티티에 Account 컬럼 정보JsonIgnore 설정

--- backend/src/main/java/com/toffeestory/backend/post/Post.java
+++ backend/src/main/java/com/toffeestory/backend/post/Post.java
@@ -1,5 +1,6 @@
 package com.toffeestory.backend.post;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.toffeestory.backend.account.Account;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
+@JsonIgnoreProperties("account")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
